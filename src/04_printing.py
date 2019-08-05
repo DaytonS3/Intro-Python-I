4dd243dbb5b055b15c3f42c3a554c9b404031d8f
Ben Hardin <benhardin@Bens-MacBook-Air.local>
2019-08-05 18:52:48 -0400

    part of 4 / 5 complete

--- src/04_printing.py
+++ src/04_printing.py
@@ -10,8 +10,8 @@ z = "I like turtles!"
 
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
+print("x is %d, y is %2f z is %s")
 # x is 10, y is 2.25, z is "I like turtles!"
 
 # Use the 'format' string method to print the same thing
-
 # Finally, print the same thing using an f-string
